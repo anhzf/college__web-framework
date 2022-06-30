dd1204893039600645e24d65a637bf00d4f10ebd
amalia-rizqi <amaliamarwah69@gmail.com>
2022-06-30 15:25:22 +0700

    feat(webapp):

--- webapp/src/pages/FacilityManagement.vue
+++ webapp/src/pages/FacilityManagement.vue
@@ -14,5 +14,48 @@
         </v-btn>
       </v-col>
     </v-row>
+
+    <v-row>
+      <v-col>
+        <v-table
+          height="70vh"
+          fixed-header
+          width="50vh"
+        >
+          <thead>
+            <tr>
+              <th>Nama Fasilitas</th>
+              <th>Jumlah</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Router Mikrotik</td>
+              <td>20</td>
+              <td>
+                <v-btn
+                  prepend-icon="mdi-plus"
+                  color="success"
+                >
+                  Tambah
+                </v-btn>
+                <v-btn
+                  prepend-icon="mdi-minus"
+                  color="error"
+                >
+                  Kurang
+                </v-btn>
+                <v-btn
+                  color="primary"
+                >
+                  Detail
+                </v-btn>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
--- webapp/src/pages/RoomManagement.vue
+++ webapp/src/pages/RoomManagement.vue
@@ -14,5 +14,47 @@
         </v-btn>
       </v-col>
     </v-row>
+
+    <v-row>
+      <v-col>
+        <v-table
+          height="70vh"
+          fixed-header
+        >
+          <thead>
+            <tr>
+              <th>Nama Fasilitas</th>
+              <th>Jumlah</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Ruangan Multimedia</td>
+              <td>2</td>
+              <td>
+                <v-btn
+                  prepend-icon="mdi-plus"
+                  color="success"
+                >
+                  Tambah
+                </v-btn>
+                <v-btn
+                  prepend-icon="mdi-minus"
+                  color="error"
+                >
+                  Kurang
+                </v-btn>
+                <v-btn
+                  color="primary"
+                >
+                  Detail
+                </v-btn>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
