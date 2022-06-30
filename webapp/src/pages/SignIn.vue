7c9a3b825835bd694c5ca5f0bba334e1926a0b09
amalia-rizqi <amaliamarwah69@gmail.com>
2022-06-30 14:22:31 +0700

    feat(webapp): tampilan sign-in

--- webapp/src/pages/SignIn.vue
+++ webapp/src/pages/SignIn.vue
@@ -1,14 +1,33 @@
 <template>
   <v-container fluid>
-    <v-row>
-      <v-col>
-        <h1>Masuk Member</h1>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-btn :to="{name: 'SignUp'}">
-        Buat akun
-      </v-btn>
-    </v-row>
+    <v-card
+      class="mx-auto"
+      max-width="500"
+    >
+      <v-row justify="space-between">
+        <v-col justify="space-between">
+          <v-col
+            cols="auto"
+            sm="12"
+          >
+            <h1>
+              Masuk Member
+            </h1>
+            <v-text-field
+              label="Email"
+            />
+            <v-text-field
+              label="Password"
+            />
+            <v-btn
+              :to="{name: 'SignIn'}"
+              color="primary"
+            >
+              Sign In
+            </v-btn>
+          </v-col>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
--- webapp/src/pages/SignUp.vue
+++ webapp/src/pages/SignUp.vue
@@ -1,14 +1,53 @@
 <template>
   <v-container fluid>
-    <v-row>
-      <v-col>
-        <h1>Daftar menjadi Member</h1>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-btn :to="{name:'SignIn'}">
-        Login
-      </v-btn>
-    </v-row>
+    <v-card
+      class="mx-auto"
+      max-width="500"
+    >
+      <v-row justify="space-between">
+        <v-col justify="space-between">
+          <v-col
+            cols="auto"
+            sm="12"
+          >
+            <h1>Daftar menjadi Member</h1>
+            <v-text-field
+              label="Nama"
+            />
+            <v-text-field
+              label="Email"
+            />
+            <v-text-field
+              label="Username"
+            />
+            <v-text-field
+              label="Password"
+            />
+            <v-text-field
+              label="Re-Password"
+            />
+            <v-checkbox
+              v-model="checkbox"
+              :label="`Daftar sebagai Civitas UNS`"
+            />
+            <v-text-field
+              label="NIM Mahasiswa/NIP Dosen"
+            />
+            <v-btn
+              :to="{name:'SignUp'}"
+              color="primary"
+            >
+              Sign Up
+            </v-btn>
+          </v-col>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const checkbox = ref(true);
+</script>
