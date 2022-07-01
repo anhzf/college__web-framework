c65b8e2c6422e9ecdcf71ef673ecd60a4f579fb5
Alviana03 <alviana.hermawati1261@student.uns.ac.id>
2022-07-01 17:53:15 +0700

    feat(webapp): :art: Nambah Checkbox

--- webapp/src/pages/SignIn.vue
+++ webapp/src/pages/SignIn.vue
@@ -4,7 +4,7 @@
       class="mx-auto"
       max-width="500"
     >
-      <v-row justify="space-between">
+      <v-row>
         <v-col justify="space-between">
           <v-col
             cols="auto"
@@ -15,9 +15,19 @@
             </div>
             <v-text-field
               label="Email"
+              type="email"
+              :rules="emailRules"
+              required
             />
             <v-text-field
               label="Password"
+              type="password"
+              :rules="[v => !!v || 'Password harus di isi']"
+              required
+            />
+            <v-checkbox
+              v-model="checkbox"
+              label="Tetap Masuk"
             />
             <v-btn
               :to="{name: 'SignIn'}"
@@ -25,7 +35,7 @@
             >
               Sign In
             </v-btn>
-            <p><a href="">Belum punya akun? Klik disini</a></p>
+            <p><a href="/signup">Belum punya akun? Klik disini</a></p>
             <p><a href="">Lupa Password?</a></p>
           </v-col>
         </v-col>
@@ -33,6 +43,18 @@
     </v-card>
   </v-container>
 </template>
+<script>
+export default {
+  data: () => ({
+    email: '',
+    emailRules: [
+      (v) => !!v || 'Harap isi E-mail Anda',
+      (v) => /.+@.+\..+/.test(v) || 'E-mail harus valid',
+    ],
+    checkbox: false,
+  }),
+};
+</script>
 <style>
 .header {
   text-align: center;
