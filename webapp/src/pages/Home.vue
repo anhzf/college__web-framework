15c5ca36499a53171502ee7d4518ff9e2c8ed2b7
Alviana03 <alviana.hermawati1261@student.uns.ac.id>
2022-07-01 11:14:12 +0700

    feat(webapp): :art: Tampilan Dashboard

--- webapp/src/pages/Home.vue
+++ webapp/src/pages/Home.vue
@@ -1,7 +1,53 @@
 <template>
   <v-container fluid>
-    <v-btn>
-      asdasd
-    </v-btn>
+    <h1>
+      DAFTAR RESERVASI ANDA
+    </h1>
+
+    <v-row>
+      <v-col>
+        <v-card
+          color="#99ccff"
+          dark
+        >
+          <v-card-title class="text-h5">
+            Software Engineering
+          </v-card-title>
+
+          <v-card-subtitle type="date-time">
+            Waktu Reservasi
+          </v-card-subtitle>
+
+          <v-card-text>
+            Deskripsi Kegiatan
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card
+          color="#FF99FF"
+          dark
+        >
+          <v-card-title class="text-h5">
+            Software Engineering
+          </v-card-title>
+
+          <v-card-subtitle type="date-time">
+            Waktu Reservasi
+          </v-card-subtitle>
+
+          <v-card-text>
+            Deskripsi Kegiatan
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
+<script>
+export default {
+  data: () => ({
+    show: false,
+  }),
+};
+</script>
